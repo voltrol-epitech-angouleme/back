51b08685495182b2585c4d0c4266b4bece524045
hienpham512 <thi-hien.pham@epitech.eu>
2023-06-12 11:05:01 +0200

    test: test deploy on netlify

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,22 @@
-import express from 'express';
+// import express from 'express';
 
-const app = express();
-const port = process.env.PORT || 3000;
+// const app = express();
+// const port = process.env.PORT || 3000;
 
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello, world!' });
+// app.get('/api', (req, res) => {
+//   res.json({ message: 'Hello, world!' });
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+import http from 'http';
+
+const server: any = http.createServer((req, res) => {
+  res.end('Hello, world!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
