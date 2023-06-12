6bd0ddae7e65f59678f490ddcdd305fe9acc9e36
hienpham512 <thi-hien.pham@epitech.eu>
2023-06-12 13:40:16 +0200

    chore: added types library to node_modules

--- src/users/index.ts
+++ src/users/index.ts
@@ -1,7 +1,7 @@
 import * as functions from "firebase-functions";
 
 export const helloWorld = functions.https.onRequest((request, response) => {
-	const userName = request.query.name;
-  functions.logger.info(`Hello logs! ${userName}`, {structuredData: true});
+  const userName = request.query.name;
+  functions.logger.info(`Hello logs! ${userName}`, { structuredData: true });
   response.send(`Hello ${userName} from Firebase!`);
 });
