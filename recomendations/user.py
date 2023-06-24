3b496ae8702dc412a2e796d38524107204f77b1c
hienpham512 <thi-hien.pham@epitech.eu>
2023-06-24 20:42:22 +0200

    fix: fixed user books recommendation when readBooks is empty

--- recomendations/user.py
+++ recomendations/user.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import random
 
 # Get the absolute path of the directory containing this script
 script_directory = os.path.dirname(os.path.abspath(__file__))
@@ -15,25 +16,32 @@ with open(file_path, "rb") as load_file:
 
 
 def get_book_recommendations_user(user, num_recommendations, books):
-    user_books = [book['id']
+    print(user)
+    try:
+        user_books = [book['id']
                   for book in books if book['id'] in user['readBooks']]
 
-    book_recommendations = []
-    for book in books:
-        book_id = book['id']
-        if book_id not in user_books:
-            predicted_rating = model.predict(user["id"], book_id).est
-            book_recommendations.append((book_id, predicted_rating))
 
-    # Sort book recommendations by predicted rating
-    book_recommendations.sort(key=lambda x: x[1], reverse=True)
-    book_recommendations = book_recommendations[:num_recommendations]
+        book_recommendations = []
+        for book in books:
+            book_id = book['id']
+            if book_id not in user_books:
+                predicted_rating = model.predict(user["id"], book_id).est
+                book_recommendations.append((book_id, predicted_rating))
 
-    books_res = []
-    for book_r in book_recommendations:
-        book_id = book_r[0]
-        book = next((book for book in books if book['id'] == book_id), None)
-        if book is not None:
-            books_res.append(book)
+        # Sort book recommendations by predicted rating
+        book_recommendations.sort(key=lambda x: x[1], reverse=True)
+        book_recommendations = book_recommendations[:num_recommendations]
 
-    return books_res
+        books_res = []
+        for book_r in book_recommendations:
+            book_id = book_r[0]
+            book = next((book for book in books if book['id'] == book_id), None)
+            if book is not None:
+                books_res.append(book)
+        return books_res
+        
+    except Exception as e:
+        book_recommendations = random.sample(books, num_recommendations)
+        return book_recommendations
+
